c5f90633c63e3ad2070540153f33be6efd10e2c3
Alexander Kataev <kataevalexanderwork@gmail.com>
2017-02-19 22:55:04 +0500

    commit

--- src/frontend/demo/script.js
+++ src/frontend/demo/script.js
@@ -16,4 +16,42 @@ function render() {
 	list.innerHTML = template
 }
 
+function validate(value) {
+	if (value.trim() !== '') {
+		return true //строка не пустая, можно продолжать
+	} else {
+		return false //реакция на пустую строку
+	}
+}
+
+function clear() {
+	input.value = ''
+}
+
+input.addEventListener('keydown', function(event) { //добавили обработчик для input
+	if (event.keyCode !== 13) {
+		return
+	}
+
+	var value = input.value //получили введенное значение
+
+	if (validate(value)) { //проверили что строка не пустая
+		model.push(value) //добавили строку в список значений
+	}
+
+	render() //вывели новый список значений
+	clear() //очистили поле ввода
+})
+
+button.addEventListener('click', function() {
+	var value = input.value
+
+	if (validate(value)) {
+		model.push(value)
+	}
+
+	render()
+	clear()
+})
+
 render()
